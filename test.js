e5e01795e89a1cd0b5ae238de89dfa8e588a9d40
Tanya <for.pm.sp@gmail.com>
2023-12-25 12:28:38 +0200

    prepare js.hw.3

---
+++ test.js
@@ -0,0 +1,43 @@
+// function createArrayOfNumbers(min, max) {
+//     let number = [];
+//     for (; min <= max; min++) {
+//         number.push(min);
+//     }
+//     return number;
+// }
+
+
+// // console.log(createArrayOfNumbers(1, 3))
+// console.log(createArrayOfNumbers(14, 17))
+
+
+// ____________________________________________________________________________________
+// function calculateTotalPrice(order) {
+//     let sum = 0;
+//     for (let i = 0; i < order.length; i++) {
+//         sum += order[i];
+//     }
+//     return sum;
+// }
+
+// console.log(calculateTotalPrice([164, 48, 291]))
+
+
+
+
+
+// ____________________________________________________________________________________________
+// function checkStorage(storage, item) {
+//     if (storage.includes(item.toLowerCase())) {
+//         let product = storage[storage.indexOf(item.toLowerCase())];
+//         return `${product} is available to order!`;
+//     }
+// }
+
+
+// console.log(checkStorage(["apple", "plum", "pear"], "pLum"))
+
+
+
+// ====================================================================================
+
